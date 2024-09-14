8a0350b60f05b9b7a9689fef1c5bf326e341f478
AserZakaria <asserzakaria@gmail.com>
2024-09-14 09:53:15 +0300

    Addition of New Pages (Courses - Shopping)

--- src/app/user-page/home-padge/course-card/course-card.component.ts
+++ src/app/user-page/home-padge/course-card/course-card.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-course-card',
@@ -8,5 +9,48 @@ import { Component } from '@angular/core';
   styleUrl: './course-card.component.scss'
 })
 export class CourseCardComponent {
+  Course: any[] = [
+    {
+      id: "1",
+      title: 'Arabic',
+      description: "Learn Arabic for Students in 3rd grade",
+      Userid: '1'
+    } ,
+
+    {
+      id: "2",
+      title: 'English',
+      description: "Learn English for Students in 3rd grade",
+      Userid: '2'
+    } ,
+
+    {
+      id: "3",
+      title: 'Math',
+      description: "Learn Math for Students in 3rd grade",
+      Userid: '3'
+    } ,
+
+    {
+      id: "4",
+      title: 'French',
+      description: "Learn French for Students in 3rd grade",
+      Userid: '4'
+    } ,
+
+    {
+      id: "5",
+      title: 'German',
+      description: "Learn German for Students in 3rd grade",
+      Userid: '5'
+    } ,
+
+    {
+      id: "6",
+      title: 'Physics',
+      description: "Learn Physics for Students in 3rd grade",
+      Userid: '6'
+    }
+  ]
 
 }
