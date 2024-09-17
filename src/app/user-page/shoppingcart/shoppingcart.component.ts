a0a642521f4ca9e5745595d43aae2cd1c83a13e0
AserZakaria <asserzakaria@gmail.com>
2024-09-17 23:04:01 +0300

    Addition of the Shoppingcart Page

--- src/app/user-page/shoppingcart/shoppingcart.component.ts
+++ src/app/user-page/shoppingcart/shoppingcart.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { CommonModule } from '@angular/common';
+import { DataViewModule } from "primeng/dataview";
+import { ButtonModule } from "primeng/button";
+import { TagModule } from "primeng/tag";
+
+interface Course {
+  title: string;
+  category: string;
+  price: number;
+  instructor: string;
+  image: string;
+}
 
 @Component({
   selector: 'app-shoppingcart',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, DataViewModule, ButtonModule, TagModule],
   templateUrl: './shoppingcart.component.html',
-  styleUrl: './shoppingcart.component.scss'
+  styleUrls: ['./shoppingcart.component.scss']
 })
 export class ShoppingcartComponent {
-
+  courses: Course[] = [
+    {
+      title: 'Introduction to Programming',
+      category: 'Computer Science',
+      price: 100,
+      instructor: 'John Doe',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Copyright.svg/480px-Copyright.svg.png'
+    },
+    {
+      title: 'Advanced Mathematics',
+      category: 'Mathematics',
+      price: 150,
+      instructor: 'Jane Smith',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Copyright.svg/480px-Copyright.svg.png'
+    },
+    {
+      title: 'Graphic Design Basics',
+      category: 'Design',
+      price: 200,
+      instructor: 'Emily Johnson',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Copyright.svg/480px-Copyright.svg.png'
+    },
+    {
+      title: 'Business Management',
+      category: 'Business',
+      price: 250,
+      instructor: 'Michael Brown',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Copyright.svg/480px-Copyright.svg.png'
+    },
+    {
+      title: 'Digital Marketing',
+      category: 'Marketing',
+      price: 300,
+      instructor: 'Sarah Davis',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Copyright.svg/480px-Copyright.svg.png'
+    }
+  ];
 }
