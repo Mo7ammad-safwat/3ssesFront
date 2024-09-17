96a4a36a8fc7ee0f6ccd3ef6375b979d2ad22555
AserZakaria <asserzakaria@gmail.com>
2024-09-17 10:45:26 +0300

    Changes in Login and SignUp for it's methods and Structure

--- src/app/security/log-in/log-in.component.ts
+++ src/app/security/log-in/log-in.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Router } from '@angular/router';
+import { MockAuthService } from '../../../app/mock-auth.service'; // Import MockAuthService
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-log-in',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, RouterModule],
   templateUrl: './log-in.component.html',
-  styleUrl: './log-in.component.scss'
+  styleUrls: ['./log-in.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class LogInComponent {
+  email: string = '';
+  password: string = '';
 
+  constructor(private authService: MockAuthService, private router: Router) {} // Inject MockAuthService and Router
+
+  onLogin() {
+    if (this.authService.login(this.email, this.password)) {
+      this.router.navigate(['/home']); // Navigate to home on successful login
+    } else {
+      alert('Invalid credentials'); // Show an alert on failed login
+    }
+  }
 }
