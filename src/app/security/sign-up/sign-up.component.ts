ca5162307afc305ba1902abd73b1e8f2f95c7311
AserZakaria <asserzakaria@gmail.com>
2024-09-17 10:45:26 +0300

    Changes in Login and SignUp for it's methods and Structure

--- src/app/security/sign-up/sign-up.component.ts
+++ src/app/security/sign-up/sign-up.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
   imports: [],
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.scss'
+  styleUrl: './sign-up.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class SignUpComponent {
 
