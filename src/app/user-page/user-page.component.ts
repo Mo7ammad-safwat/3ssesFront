1905c4f8723f28ef99d59c8f7b304e060f3e20a2
AserZakaria <asserzakaria@gmail.com>
2024-09-17 10:46:12 +0300

    Changes in Sidebar to stop it from appearing in the SignUp/Login

--- src/app/user-page/user-page.component.ts
+++ src/app/user-page/user-page.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
-import { SidebarComponent } from "../core/sidebar/sidebar.component";
 import { Router, RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { HeadrComponent } from "../core/sidebar/headr/headr.component";
+import { SidebarComponent } from '../core/sidebar/sidebar.component';
+import { HeadrComponent } from "../../app/core/sidebar/headr/headr.component"; // Import SidebarComponent
 
 @Component({
   selector: 'app-user-page',
   standalone: true,
-  imports: [SidebarComponent, RouterOutlet, FormsModule, CommonModule, HeadrComponent],
+  imports: [RouterOutlet, FormsModule, CommonModule, SidebarComponent, HeadrComponent], // Import SidebarComponent
   templateUrl: './user-page.component.html',
-  styleUrl: './user-page.component.scss',
+  styleUrls: ['./user-page.component.scss'],
 })
 export class UserPageComponent {
   isSidebarHovered: boolean = false;
