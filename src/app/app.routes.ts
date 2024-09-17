98d0dea4801fb3a208981e8bf69a95980c52fd8e
AserZakaria <asserzakaria@gmail.com>
2024-09-17 10:43:15 +0300

    Addition of Navigation from Login to /home After Successfully logging in

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { UserPageComponent } from './user-page/user-page.component';
 import { AdminComponent } from './admin/admin.component';
 import { ShoppingcartComponent } from './user-page/shoppingcart/shoppingcart.component';
 import { MyLearnComponent } from './user-page/my-learn/my-learn.component';
-import { HomePadgeComponent } from './user-page/home-padge/home-padge.component';
+import { HomePadgeComponent } from '../app/user-page/home-padge/home-padge.component';
 import { CoursesComponent } from './user-page/courses/courses.component';
 import { PurchasedCoursesComponent } from './user-page/home-padge/purchased-courses/purchased-courses.component';
+import { SignUpComponent } from './security/sign-up/sign-up.component';
+import { LogInComponent } from './security/log-in/log-in.component';
 
 export const routes: Routes = [
+  { path: 'login', component: LogInComponent },
+  { path: 'signup', component: SignUpComponent },
   {
     path: 'home',
     component: UserPageComponent,
@@ -20,6 +24,6 @@ export const routes: Routes = [
     ],
   },
   { path: 'admin', component: AdminComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: '**', pathMatch: 'full', component: UserPageComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Set login as the default route
+  { path: '**', pathMatch: 'full', component: LogInComponent },
 ];
